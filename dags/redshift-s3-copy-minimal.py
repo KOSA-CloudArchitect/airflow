90c4d74b1aefcb09d734960bd29da22444b7f6eb
TJ Park <hahxowns@naver.com>
2025-09-21 00:04:45 +0900

    DEBUG: 연결 정보 수정

--- dags/redshift-s3-copy-minimal.py
+++ dags/redshift-s3-copy-minimal.py
@@ -166,7 +166,7 @@ copy_to_redshift = RedshiftDataOperator(
     params={
         'schema': REDSHIFT_SCHEMA,
         'table': REDSHIFT_TABLE,
-        'iam_role': 'arn:aws:iam::914215749228:role/hihypipe-redshift-s3-copy-role'
+        'iam_role': 'arn:aws:iam::914215749228:role/hihypipe-airflow-irsa'
     },
     aws_conn_id='aws_default',
     retries=3,
